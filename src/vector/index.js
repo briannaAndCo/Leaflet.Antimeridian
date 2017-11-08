9b2bbe29ee9ac3902c95702f6233a765657d4b91
landonbj <brianna.landon@sbcglobal.net>
2017-11-08 13:38:13 -0800

    Added intital page commit.

--- src/vector/index.js
+++
@@ -1,3 +0,0 @@
-export {Polyline, wrappedPolyline} from './Wrapped.Polyline';
-export {Polygon, wrappedPolygon} from './Wrapped.Polygon';
-export {calculateAntimeridianLat, isCrossMeridian, sign, isBreakRing} from './Wrapped.LineUtils';
