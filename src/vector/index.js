324a2e15832ca65790b355715510255d5444d383
landonbj <brianna.landon@sbcglobal.net>
2017-11-28 16:58:26 -0800

    Committing to tag.

--- src/vector/index.js
+++
@@ -1,4 +0,0 @@
-export {Polyline, wrappedPolyline} from './Wrapped.Polyline';
-export {Polygon, wrappedPolygon} from './Wrapped.Polygon';
-export {calculateAntimeridianLat, isCrossMeridian, isBreakRing} from './Wrapped.AntimeridianUtils';
-export {sign} from './Wrapped.MathUtils';
